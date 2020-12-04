1b3851e40c294629b6a3d17bf04cec40409892e6
Vulcan <vulcan.lee@gmail.com>
2020-12-04 23:54:17 +0800

    使用 Task.Delay 來取代封鎖 Block 的 Thread.Sleep

---
+++ PF07/PF07/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PF07
+{
+    class Program
+    {
+        static List<(DateTime current, int NumberOfThreads)> threadUsage =
+            new List<(DateTime current, int NumberOfThreads)>();
+        static void Main(string[] args)
+        {
+            int MAX = 10000;
+            int SLEEP = 5 * 1000;
+            bool stopMonitor = false;
+            DateTime now = DateTime.Now;
+            List<Task> tasks = new List<Task>();
+
+            #region 建立與統計最多執行緒數量的執行緒
+            Thread monitorWorker = new Thread(() =>
+            {
+                while (!stopMonitor)
+                {
+                    Thread.Sleep(200);
+                    threadUsage.Add((DateTime.Now,
+                        Process.GetCurrentProcess().Threads.Count));
+                }
+            });
+            monitorWorker.Start();
+            #endregion
+
+            #region 建立紀錄每個執行緒開始執行會延遲多少時間
+            List<double> delay = new List<double>();
+            for (int i = 0; i < MAX; i++) { delay.Add(0); }
+            #endregion
+
+            #region 啟動並且執行10000個 Task
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            now = DateTime.Now;
+            for (int i = 0; i < MAX; i++)
+            {
+                int idx = i;
+                Task task = Task.Run(async () =>
+                {
+                    delay[idx] = (DateTime.Now - now).TotalMilliseconds;
+                    await Task.Delay(SLEEP);
+                });
+                tasks.Add(task);
+            }
+            #endregion
+
+            Task.WaitAll(tasks.ToArray());
+            stopwatch.Stop();
+            stopMonitor = true;
+            Console.WriteLine();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+
+            Console.WriteLine($"Max {threadUsage.Max(x => x.NumberOfThreads)} Threads");
+
+            foreach (var item in delay)
+            {
+                Console.Write($" {item} ");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 |範例體驗|PF04|了解 10000 執行緒的每個執行緒執行所需時間|
 |範例體驗|PF05|聽說 Task 很厲害，那就使用 10000 Tasks(Task.Run)|
 |範例體驗|PF06|用Task.Factory.StartNew 建立 10000 Tasks|
-||||
+|範例體驗|PF07|使用 Task.Delay 來取代封鎖 Block 的 Thread.Sleep|
 ||||
 ||||
 ||||
