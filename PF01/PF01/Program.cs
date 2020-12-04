987f6c423d9cfdce477c0adcce66bc8bb0b45520
Vulcan <vulcan.lee@gmail.com>
2020-12-04 19:37:32 +0800

    同時執行 10000 次

---
+++ PF01/PF01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PF01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.For(0, 10000, _ =>
+            {
+                Thread.Sleep(5 * 1000);
+            });
+            stopwatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
