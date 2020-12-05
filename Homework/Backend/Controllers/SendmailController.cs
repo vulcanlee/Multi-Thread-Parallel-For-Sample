5acf1a0404d6882c85c146d1f166ecc6550f9758
Vulcan <vulcan.lee@gmail.com>
2020-12-05 22:23:20 +0800

    使用Parallel.For方法，呼叫 Web API 做兩數相加

---
+++ Homework/Backend/Controllers/SendmailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SendmailController : ControllerBase
+    {
+        public SendmailController()
+        {
+        }
+
+        [HttpGet("{value1}/{value2}")]
+        public async Task<string> Get(int value1, int value2)
+        {
+            string result = "" ;
+            result = (value1 + value2).ToString();
+            Console.WriteLine(result);
+            await Task.Delay(1000);
+            return result;
+        }
+    }
+}
---
+++ Homework/Frontend/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MAX = 100;
+            string endPoint = "https://localhost:5001/Sendmail/";
+            HttpClient client = new HttpClient();
+            ConcurrentDictionary<int, string> callAPIResult = new ConcurrentDictionary<int, string>();
+            List<int> cannotUseConcurrent = new List<int>();
+            CountdownEvent cde = new CountdownEvent(MAX);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Parallel.For(0, MAX, async (i) =>
+            {
+                int idx = i;
+                string result = await client
+                .GetStringAsync($"{endPoint}{idx}/{idx}");
+                callAPIResult.TryAdd(idx, result);
+                cannotUseConcurrent.Add(idx);
+                cde.Signal();
+            });
+
+            cde.Wait();
+            stopwatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine();
+
+            foreach ((int index, string result) in callAPIResult)
+            {
+                Console.WriteLine($"Index={index} > {result}");
+            }
+        }
+    }
+}
