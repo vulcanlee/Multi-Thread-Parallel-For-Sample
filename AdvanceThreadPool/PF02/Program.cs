0dd282ab7a3d260220bbf6099600ce1332847303
Vulcan <vulcan.lee@gmail.com>
2020-12-08 15:08:32 +0800

    調整 ThreadPool 的條件

---
+++ AdvanceThreadPool/PF02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PF02
+{
+    class Program
+    {
+        static List<(DateTime current, int NumberOfThreads)> threadUsage =
+            new List<(DateTime current, int NumberOfThreads)>();
+        static void Main(string[] args)
+        {
+            int MAX = 10000;
+            int SLEEP = 5 * 1000;
+            bool stopMonitor = false;
+
+            #region 調整 ThreadPool 的條件
+            int workerThreads, completionPortThreads;
+            ThreadPool.GetMaxThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"ThreadPool Max Threads {workerThreads} / {completionPortThreads}");
+            ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"ThreadPool Min Threads {workerThreads} / {completionPortThreads}");
+            workerThreads = 10000;
+            ThreadPool.SetMinThreads(workerThreads, completionPortThreads);
+            ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"ThreadPool Min Threads {workerThreads} / {completionPortThreads}");
+            #endregion
+
+            #region 建立與統計最多執行緒數量的執行緒
+            Thread monitorWorker = new Thread(() =>
+            {
+                while (!stopMonitor)
+                {
+                    Thread.Sleep(200);
+                    threadUsage.Add((DateTime.Now,
+                        Process.GetCurrentProcess().Threads.Count));
+                }
+            });
+            monitorWorker.Start();
+            #endregion
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.For(0, MAX, _ =>
+            {
+                Thread.Sleep(SLEEP);
+            });
+            stopwatch.Stop();
+            stopMonitor = true;
+            Console.WriteLine();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+
+            Console.WriteLine($"Max {threadUsage.Max(x=>x.NumberOfThreads)} Threads");
+        }
+    }
+}
