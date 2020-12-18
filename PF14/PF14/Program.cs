474d593d0edf29f18f7b983258f432d0e7356428
Vulcan <vulcan.lee@gmail.com>
2020-12-18 18:59:18 +0800

    PF14|Parallel.For 了解執行緒集區的運作方式

---
+++ PF14/PF14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PF14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Now:{DateTime.Now}");
+            #region 平行處理 200 次迴圈，了解執行緒集區的運作方式
+            Parallel.For(0, 200, (i) =>
+            {
+                Console.Write($"*");
+                // 模擬等待 5 秒鐘
+                Thread.Sleep(5000);
+            });
+            #endregion
+            // 請觀察開始執行時間與結束時間輸出值
+            Console.WriteLine("");
+            Console.WriteLine($"Now:{DateTime.Now}");
+        }
+    }
+}
