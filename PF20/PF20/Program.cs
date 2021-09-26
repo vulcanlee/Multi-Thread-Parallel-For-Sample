1a6a9f3739a6267e2f2dd7cc11a6e330f73907ca
Vulcan <vulcan.lee@gmail.com>
2021-09-26 13:51:34 +0800

    使用 100 Task 完成呼叫 100 次 Web API

---
+++ PF20/PF20/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF20
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int cost = 5000;
+            int MaxTasks = 100;
+            string APIEndPoint = $"https://businessblazor.azurewebsites.net/api/RemoteService/AddAsync/8/9/{cost}";
+            //string APIEndPoint = $"https://businessblazor.azurewebsites.net/api/RemoteService/Add/8/9/{cost}";
+
+            Stopwatch stopwatch = new Stopwatch(); stopwatch.Start();
+
+            #region 使用 for
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < MaxTasks; i++)
+            {
+                int idx = i;
+                tasks.Add(Task.Run(async () =>
+                {
+                    HttpClient client = new HttpClient();
+                    DateTime begin = DateTime.Now;
+                    string result = await client.GetStringAsync(
+                        APIEndPoint);
+                    DateTime complete = DateTime.Now;
+                    TimeSpan total = complete - begin;
+                    Console.WriteLine($"{idx:D3} {begin:ss}-{complete:ss}={total.TotalSeconds:N3}    {result}");
+                }));
+            }
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+            #endregion
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
