8716476297ad66c8f1ef9f3ecf8bf818208865a0
Vulcan <vulcan.lee@gmail.com>
2021-10-05 10:57:05 +0800

    在此設定中斷點，用來觀察執行緒與與工作的數量有多少

--- PF02/PF02/Program.cs
+++ PF02/PF02/Program.cs
@@ -17,6 +17,19 @@ namespace PF02
             int SLEEP = 5 * 1000;
             bool stopMonitor = false;
 
+            #region 在此設定中斷點，用來觀察執行緒與與工作的數量有多少
+
+            var timer = new System.Timers.Timer();
+            // 調整要觀察的時間點
+            timer.Interval = 17000;
+            timer.Elapsed += (s, e) =>
+            {
+                // 請宣告底下這行為除錯中斷點
+                int foo = 999;
+            };
+            timer.Enabled = true; timer.Start();
+            #endregion
+
             #region 建立與統計最多執行緒數量的執行緒
             Thread monitorWorker = new Thread(() =>
             {
